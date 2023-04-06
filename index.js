b5ccade917b06a236a45d2c9f9836d1b28edcaf2
Phillip Harden <phillipharden@Phillips-MacBook-Pro.local>
2023-04-06 12:49:36 -0400

    GET and POST

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 // GET / HTTP/1.1
 app.get('/', (request, response) => {
-    response.send('Home Page!')
+    response.send('Home Page! GET...')
+})
+
+app.post('/', (request, response) => {
+    response.send('Home Page! POST...')
 })
 
 app.listen(3000) // http://localhost:3000/ in the web browser
