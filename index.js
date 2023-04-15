71fd6b8cd728630274922bf4a6150eaa02053228
Phillip Harden <phillipharden@Phillips-MacBook-Pro.local>
2023-04-15 14:50:22 -0400

    Variables, Layouts, and Partials complete

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
+const { Quiz } = require("./src/models");
 const quizzesCtrl = require("./src/controllers/quizzes");
 const questionsCtrl = require("./src/controllers/questions");
 const choicesCtrl = require("./src/controllers/choices");
 
+app.set("views", __dirname + "/src/views");
+app.set("view engine", "twig");
+
 // GET / HTTP/1.1
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", async (req, res) => {
+  const quiz = await Quiz.findByPk(1);
+  res.render("home/home", { quiz });
 });
 
 app.use("/quizzes", quizzesCtrl);
